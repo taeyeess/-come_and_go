7ecc49032ee219702b79378efd2d550449b9807a
taeyeess <taeyeeya@gmail.com>
2023-01-04 11:27:21 +0900

    style: input checkbox label style변경

--- src/components/receipt/CheckInput.js
+++ src/components/receipt/CheckInput.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const CheckInput = () => {
-  return <input type="checkbox"></input>;
+  return (
+    <>
+      <input type="checkbox" id="checked" style={{ display: "none" }}></input>
+      <label
+        htmlFor="checked"
+        style={{
+          display: "inline-block",
+          width: "10px",
+          height: "10px",
+          border: "1px solid black",
+          backgroundColor: "green",
+          // backgroundImage: "url(../../../../../public/images/checked.png)",
+          cursor: "pointer",
+          margin: "0 4px",
+        }}
+      ></label>
+    </>
+  );
 };
 
 export default CheckInput;
--- src/components/receipt/CheckOutInput.js
+++ src/components/receipt/CheckOutInput.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const CheckOutInput = () => {
-  return <input type="checkbox"></input>;
+  return (
+    <>
+      <input type="checkbox" id="checkout" style={{ display: "none" }}></input>
+      <label
+        htmlFor="checkout"
+        style={{
+          display: "inline-block",
+          width: "10px",
+          height: "10px",
+          border: "1px solid black",
+          cursor: "pointer",
+          margin: "0 4px",
+        }}
+      ></label>
+    </>
+  );
 };
 
 export default CheckOutInput;
