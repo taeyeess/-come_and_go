70baf1b5558476d877b20e9f1b7ad3dc9030891f
taeyeess <taeyeeya@gmail.com>
2023-02-10 17:43:33 +0900

    style: style수정

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <Link to="/">
+      <Link to="/" style={{ textDecoration: "none" }}>
         <h1>DEULOCK - NALOCK</h1>
       </Link>
     </>
